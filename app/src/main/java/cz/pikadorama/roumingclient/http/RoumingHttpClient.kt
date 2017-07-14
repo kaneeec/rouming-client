dbc90fd41392077382e49f1f550c7a8918cd1aa7
Tomas Zeman <tomas.zeman@oracle.com>
2017-07-15 01:31:23 +0200

    Latest and top topics parsing

--- app/src/main/java/cz/pikadorama/roumingclient/http/RoumingHttpClient.kt
+++ app/src/main/java/cz/pikadorama/roumingclient/http/RoumingHttpClient.kt
@@ -11,25 +11,37 @@ import java.net.InetSocketAddress
 import java.net.Proxy
 import java.net.URL
 
+
 class RoumingHttpClient(val context: Context) {
 
-    enum class RoumingTimeRange(postParamValue: Int) {
+    enum class RoumingTimeRange(val postParamValue: Int) {
         DAY(1),
         WEEK(2),
-        MONTH(3)
+        MONTH(3),
+        THREE_MONHTS(4),
+        SIX_MONTHS(5),
+        YEAR(6),
+        INFINITY(7)
     }
 
-    fun fetchLatest(onSuccess: Response.Listener<String>, onError: Response.ErrorListener, limit: Int = 20) {
+    fun fetchLatest(onSuccess: Response.Listener<String>, onError: Response.ErrorListener) {
         val url = "http://www.rouming.cz"
         val request = StringRequest(Request.Method.GET, url, onSuccess, onError)
-        Volley.newRequestQueue(context, ProxyStack()).add(request)
+        createRequestQueue().add(request)
     }
 
-    fun fetchTop(onSuccess: Response.Listener<String>, onError: Response.ErrorListener, limit: Int = 20, timeRange: RoumingTimeRange = RoumingTimeRange.DAY) {
-        val url = "http://www.rouming.cz/top?"
-        val request = StringRequest(Request.Method.GET, url, onSuccess, onError)
-        Volley.newRequestQueue(context, ProxyStack()).add(request)
+    fun fetchTop(onSuccess: Response.Listener<String>, onError: Response.ErrorListener,
+                 timeRange: RoumingTimeRange = RoumingTimeRange.DAY) {
+        val url = "http://www.rouming.cz/roumingListTop.php"
+        val request = object : StringRequest(Request.Method.POST, url, onSuccess, onError) {
+            override fun getParams(): Map<String, String> = mapOf(Pair("count", "1"), Pair("operation", "1"),
+                                                                  Pair("interval", timeRange.postParamValue.toString()))
+        }
+        createRequestQueue().add(request)
     }
+
+    //    private fun createRequestQueue() = Volley.newRequestQueue(context, ProxyStack())
+    private fun createRequestQueue() = Volley.newRequestQueue(context)
 
     inner class ProxyStack : HurlStack() {
         override fun createConnection(url: URL): HttpURLConnection {
